3fbf4977dbcaba17239dd7f8ce397c9f25b819a1
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2017-07-22 11:04:25 +0700

    Initial commit

---
+++ resources/assets/js/components/Example.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Example Component</div>
+
+                    <div class="panel-body">
+                        {{ title }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    // load mapState first
+    import { mapState } from 'vuex';
+
+    export default {
+        name: 'Example',
+
+        mounted () {
+            // change title dynamically
+            this.$store.commit('SET_TITLE', 'Hello Laravel, Vue, and Vuex')
+        },
+
+        computed: mapState({
+            // read state from store
+            title: state => state.title
+        })
+    }
+</script>
---
+++ resources/assets/js/store.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const state = {
+  title: ''
+}
+
+const getters = {
+
+}
+
+const mutations = {
+  SET_TITLE (state, title) {
+    state.title = title
+  }
+}
+
+const actions = {
+    
+}
+
+export default new Vuex.Store({
+  state,
+  getters,
+  mutations,
+  actions
+})
